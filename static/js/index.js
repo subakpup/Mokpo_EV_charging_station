4be619d7abbd0aa93a5984a3fe0431c8c0a9f574
hjsu0825 <hjsu0825@naver.com>
2023-12-26 06:01:17 +0900

    [Feat] 예약 확인 후 메인화면

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const reservationButton = document.getElementById('reservationButton');
-    if (reservationButton) {
-        reservationButton.addEventListener('click', function (event) {
-            event.preventDefault();
-            window.location.href = '/map'; // 이동하고자 하는 URL 경로
-        });
-    }
+    reservationButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.location.href = '/map'; // 이동하고자 하는 URL 경로
+    });
+    
 });
--- static/js/reservation.js
+++ static/js/reservation.js
@@ -1,3 +1,8 @@
-function submitReservation() {
-    alert('예약이 완료되었습니다!');
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const submitReservation = document.getElementById('submitReservation');
+    submitReservation.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('예약이 완료되었습니다.');
+        window.location.href = '/';
+    });
+});

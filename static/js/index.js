4c8602cda6e4d22a04a1a59ea64278be5bcd8801
hjsu0825 <hjsu0825@naver.com>
2024-12-23 17:30:41 +0900

    [Refactor] 코드 리팩토링

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const reservationButton = document.getElementById('reservationButton');
     reservationButton.addEventListener('click', function(event) {
         event.preventDefault();
-        window.location.href = '/map'; // 이동하고자 하는 URL 경로
+        window.location.href = '/map';
     });
     
 });
--- static/js/map.js
+++ static/js/map.js
@@ -4,7 +4,7 @@ var map = new naver.maps.Map('map', {
 });
 
 function readCSVFile() {
-    fetch('/static/data/charger.csv') // CSV 파일 경로 설정
+    fetch('/static/data/charger.csv')
         .then(response => response.text())
         .then(data => {
             parseCSVData(data);
@@ -12,7 +12,7 @@ function readCSVFile() {
 }
 
 function parseCSVData(data) {
-    const rows = data.split('\n'); // 각 행을 분리
+    const rows = data.split('\n');
 
     // 헤더 행을 제외하고 데이터 처리
     for (let i = 1; i < rows.length; i++) {
@@ -20,18 +20,16 @@ function parseCSVData(data) {
 
         const chargingStation = columns[0]; // 충전소명
         const chargerID = columns[1]; // 충전기ID
-        const chargerType = columns[2]; // 충전기
+        const chargerType = columns[2]; // 충전기 종류
         const address = columns[3]; // 주소
         const operatingHours = columns[4]; // 이용시간
         const latitude = parseFloat(columns[5]); // 위도
         const longitude = parseFloat(columns[6]); // 경도
 
-        // 위에서 추출한 정보로 마커 생성
         const markerOptions = {
             position: new naver.maps.LatLng(longitude, latitude),
-            map: map, // map 변수는 이미 생성된 지도 객체를 나타냅니다.
-            title: chargingStation // 마커에 표시할 제목
-            // 그 외에 마커에 관련된 다양한 설정을 추가할 수 있습니다.
+            map: map,
+            title: chargingStation
         };
 
         // 마커 생성
@@ -52,7 +50,6 @@ function parseCSVData(data) {
     }
 }
 
-// 각 마커에 정보 창을 연결하는 함수
 function attachInfoWindow(marker, content) {
     // 정보 창 생성
     const infoWindow = new naver.maps.InfoWindow({
@@ -74,7 +71,7 @@ function attachInfoWindow(marker, content) {
 
     const reserveBtn = document.createElement('button');
     reserveBtn.textContent = '예약';
-    reserveBtn.style.display = 'block'; // 버튼을 블록 레벨 요소로 표시하여 줄 바꿈이 일어나게 함
+    reserveBtn.style.display = 'block';
     reserveBtn.classList.add('reservation-button');
     reserveBtn.onclick = function () {
         window.location.href = '/reservation';
